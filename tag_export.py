bf61846d67a95e5880e7b43ee44cb433a801070b
E. Krabs <Tyler.Smith11407@gmail.com>
2021-10-25 18:48:27 -0400

    Update tag_export.py

--- tag_export.py
+++ tag_export.py
@@ -1,16 +1,21 @@
-directory = 'C:/'
-with open('{}e621-total-m10-d24-y21.json'.format(directory), 'r') as f:
-    r = f.read()
+import requests
+from requests.auth import HTTPBasicAuth
+import time
+import json
 
-data = json.loads(r)
-with open('{}tag-out.json'.format(directory), 'w') as o:
+directory = 'C:/'
+with open('{}e621-total-2021-10-25.json'.format(directory), 'r') as f:
+    data = json.load(f)
+
+with open('{}tag-out-2021-10-25.json'.format(directory), 'w') as o:
     o.write('[')
-    runs = 0
-    for item in data['posts']:
+    run = 1
+    for item in data:
         tags = item['tags']
-        o.write(json.dumps(tags, indent=2))
-        o.write(',')
-        print('#{}, dumped {1}'.format())
-        runs += 1
+        for tag in tags:
+            o.write(json.dumps(tags, indent=2))
+            o.write(',')
+            print('Item #{}, Tags Dumped'.format(run))
+            run += 1
     o.write(']')
     
