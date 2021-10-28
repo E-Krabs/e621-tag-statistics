ce7fe14dde016586c4ff1da3f913678889a16a35
E. Krabs <Tyler.Smith11407@gmail.com>
2021-10-27 21:58:53 -0400

    Add files via upload

---
+++ date_count.py
@@ -0,0 +1,32 @@
+import json
+import collections
+import pandas as pd
+import matplotlib.pyplot as plt
+
+directory = 'C:/Scripts/Python/[adjective][species]/'
+with open('{}date-out.json'.format(directory)) as f:
+	data = f.read().split(',')
+
+	yearcount = {}
+	years = []
+	posts = []
+	for item in data:
+		year = item.split('-')[0][1:]
+		if year not in yearcount:
+			yearcount[year] = 1
+		else:
+			yearcount[year] += 1
+	word_counter = collections.Counter(yearcount)
+	with open('{}year_count.txt'.format(directory), 'w', encoding='utf-8') as o:
+		for year, count in word_counter.most_common():
+			o.write('{0}: {1}\n'.format(year, count))
+			years.append(year)
+			posts.append(count)
+
+	plt.plot(years, posts, color='red', label='Count')
+	plt.title('Posted Per Year (2008-2021)')
+	plt.xlabel('Year')
+	plt.ylabel('Posts')
+	plt.grid()
+	plt.legend()
+	plt.show()
--- date_export.py
+++ date_export.py
@@ -6,13 +6,13 @@ with open('{}e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
     data = json.load(f)
 
 print('Exporting...')
-with open('{}id-out.json'.format(directory), 'w') as o:
+with open('{}date-out.json'.format(directory), 'w') as o:
     o.write('[')
     run = 0
     for item in data:
-        ids = item['id']
-        o.write(json.dumps(ids))
+        date = item['created_at']
+        o.write(json.dumps(date))
         o.write(',')
-        #print('Item #{}, Ids Dumped'.format(run))
+        #print('Item #{}, Dates Dumped'.format(run))
         run += 1
     o.write(']')
