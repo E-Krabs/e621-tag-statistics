a5a55708c2f4b3f3cedf96cb5307558cdcc396c1
E. Krabs <Tyler.Smith11407@gmail.com>
2021-11-03 09:31:16 -0400

    Update tag-popularity-test.py

--- tag-popularity-test.py
+++ tag-popularity-test.py
@@ -1,6 +1,9 @@
 import json
 import pandas as pd
 
+tag1 = input('First Tag to Compare:')
+tag2 = input('Second Tag to Compare:')
+
 print('Loading JSON...')
 directory = 'C:/Scripts/Python/[adjective][species]/'
 with open('{}e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
@@ -24,16 +27,18 @@ with open('{}e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
 
 	lst = []
 	for key in dic:
-		r = dic[key]['anthro']
-		print('{}: {}'.format(key, r))
-		lst.append(dic(zip(key, r)))
+		r1 = dic[key]['{}'.format(tag1)]
+		r2 = dic[key]['{}'.format(tag2)]
+		print('{}: {}-{}, {}-{}'.format(key, tag1, r1, tag2, r2))
+		lst.append(dic(zip(key, r1, r2)))
 	print(lst)	
 		
 	#https://pythonguides.com/python-plot-multiple-lines/#Python_plot_multiple_lines_from_dataframe
 		
 	#print('Plotting Popularity...')
-	#df = pd.DataFrame(lst, columns = ['Date', 'Count'])
-	#df.plot.bar(x='Date',y='Count')
-	#plt.title('Characters (957,625)')
+	#df = pd.DataFrame(lst, columns = ['Date', '{}'.format(tag1), '{}'.format(tag2)])
+	#df.plot(x='Date', y='{}'.format(tag1))
+	#df.plot(x='Date', y='{}'.format(tag2))
+	#plt.title('Popularity of {} & {}'.format(tag1, tag2))
 	#plt.show()
-	#plt.savefig('{}popularity_tag_plot.png'.format(directory), dpi=300, bbox_inches='tight') #transparent=True
+	#plt.savefig('{}{}_{}_plot.png'.format(directory, tag1, tag2), dpi=300, bbox_inches='tight') #transparent=True
