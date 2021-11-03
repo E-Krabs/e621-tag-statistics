2f2224aa3463c9965583fdaacbd1e194d3a50051
E. Krabs <Tyler.Smith11407@gmail.com>
2021-11-03 17:27:30 -0400

    Update tag-popularity-test.py

--- tag-popularity-test.py
+++ tag-popularity-test.py
@@ -1,14 +1,18 @@
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates
+from datetime import datetime
 
-tag1 = input('First Tag to Compare:')
-tag2 = input('Second Tag to Compare:')
+tag1 = input('First Tag to Compare: ')
+tag2 = input('Second Tag to Compare: ')
 
 print('Loading JSON...')
 directory = 'C:/Scripts/Python/[adjective][species]/'
 with open('{}e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
 	data = json.load(f)
 
+	print('Counting...')
 	dic = {}
 	for key in data:
 		y = key['created_at'].split('-')[0]
@@ -31,16 +35,18 @@ with open('{}e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
 		r1 = dic[key]['{}'.format(tag1)]
 		r2 = dic[key]['{}'.format(tag2)]
 		print('{}: {}-{}, {}-{}'.format(key, tag1, r1, tag2, r2))
-		lst.append(dic(zip(key, r1, r2)))
-	print(lst)	
+		fr95 = (key, r1, r2)
+		lst.append(fr95)
 		
-	#https://pythonguides.com/python-plot-multiple-lines/#Python_plot_multiple_lines_from_dataframe
-		
+	#https://pythonguides.com/python-plot-multiple-lines/#Python_plot_multiple_lines_from_dataframe	
 	print('Plotting Popularity...')
 	df = pd.DataFrame(lst, columns = ['Date', '{}'.format(tag1), '{}'.format(tag2)])
-	df.plot(x='Date', y='{}'.format(tag1), color='g', label='{}'.format(tag1))
-	df.plot(x='Date', y='{}'.format(tag2), color='r', label='{}'.format(tag2))
+	df = df.sort_values(by=['Date'], ascending=True)
+	plt.plot(df['Date'], df['{}'.format(tag1)], color='g', label='{}'.format(tag1))
+	plt.plot(df['Date'], df['{}'.format(tag2)], color='r', label='{}'.format(tag2))
 	plt.title('Popularity of {} & {}'.format(tag1, tag2))
+	plt.xticks(rotation=60)
+
 	plt.legend()
 	plt.show()
-	#plt.savefig('{}{}_{}_plot.png'.format(directory, tag1, tag2), dpi=300, bbox_inches='tight') #transparent=True
+	plt.savefig('{}{}_{}_plot.png'.format(directory, tag1, tag2)) #transparent=True
