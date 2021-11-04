676103d0d98a99c16dc31cdceadfe8bdc506fb60
E. Krabs <Tyler.Smith11407@gmail.com>
2021-11-03 20:55:34 -0400

    Update zootopia.py

--- zootopia.py
+++ zootopia.py
@@ -8,7 +8,7 @@ tag1 = 'nick_wilde'
 tag2 = 'judy_hopps'
 tag3 = 'fox'
 tag4 = 'rabbit'
-
+tag5 = 'zootopia'
 print('Loading JSON...')
 directory = 'C:/Scripts/Python/[adjective][species]/'
 with open('{}JSON/e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
@@ -25,6 +25,7 @@ with open('{}JSON/e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
 			dic[created_at] = {}
 		general = key['tags']['character']
 		species = key['tags']['species']
+		copyright = key['tags']['copyright']
 
 		for word in general:
 			if word not in dic['{}'.format(created_at)]:
@@ -37,6 +38,13 @@ with open('{}JSON/e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
 				dic['{}'.format(created_at)][word] = 1
 			else:
 				dic['{}'.format(created_at)][word] += 1
+
+		for word in copyright:
+			if word not in dic['{}'.format(created_at)]:
+				dic['{}'.format(created_at)][word] = 1
+			else:
+				dic['{}'.format(created_at)][word] += 1
+
 
 	#with open('{}'.format(directory), 'w') as o:
 	lst = []
@@ -57,18 +65,23 @@ with open('{}JSON/e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
 			r4 = dic[key]['{}'.format(tag4)] #result 4
 		except:
 			r4 = 0
-		print('{}: {}-{}, {}-{}'.format(key, tag1, r1, tag2, r2, tag3, r3, tag4, r4))
-		tr = (key, r1, r2, r3, r4) #tupled result
+		try:
+			r5 = dic[key]['{}'.format(tag5)] #result 4
+		except:
+			r5 = 0
+		print('{}: {}-{}, {}-{}'.format(key, tag1, r1, tag2, r2, tag3, r3, tag4, r4, tag5, r5))
+		tr = (key, r1, r2, r3, r4, r5) #tupled result
 		lst.append(tr)
 		
 	print('Plotting Popularity...')
-	df = pd.DataFrame(lst, columns = ['Date', '{}'.format(tag1), '{}'.format(tag2), '{}'.format(tag3), '{}'.format(tag4)])
+	df = pd.DataFrame(lst, columns = ['Date', '{}'.format(tag1), '{}'.format(tag2), '{}'.format(tag3), '{}'.format(tag4), '{}'.format(tag5)])
 	df['Date'] = pd.to_datetime(df['Date'])
 	df = df.sort_values(by=['Date'], ascending=True)
 	plt.plot(df['Date'], df['{}'.format(tag1)], color='g', label='{}'.format(tag1))
 	plt.plot(df['Date'], df['{}'.format(tag2)], color='r', label='{}'.format(tag2))
 	plt.plot(df['Date'], df['{}'.format(tag3)], color='b', label='{}'.format(tag3))
 	plt.plot(df['Date'], df['{}'.format(tag4)], color='m', label='{}'.format(tag4))
+	plt.plot(df['Date'], df['{}'.format(tag5)], color='k', label='{}'.format(tag5))
 	plt.title('Zootopias Influence on Foxes and Rabbits')
 	plt.xticks(rotation=60)
 	plt.legend()
