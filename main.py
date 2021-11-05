1fe83e914c5fb92f5e0737cc9825ca6baf8bbdec
E. Krabs <Tyler.Smith11407@gmail.com>
2021-11-05 17:25:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,28 @@ from requests.auth import HTTPBasicAuth
 import time
 import json
 
-url = "https://e621.net/posts.json?limit=1000"
+url = "https://e621.net/posts.json?limit=320"
 e621_agent = {
-   'User-Agent': '', #project_name (by username)
-   'login': '', #your username
-   'api_key': '' #your api key
+   'User-Agent': '',
+   'login': '',
+   'api_key': ''
 }
 
-login = '' #your username
-api_key = '' #your api key
+login = ''
+api_key = ''
 
-max_id = 2992268 #2992268
+max_id = 3011070 #2992268
 seen = []
 run = 0
 directory = 'C:/Scripts/Python/[adjective][species]/'
-with open('{}e621-total-2021-10-25.json'.format(directory), 'w') as f:
+''' When refreshing data, change 'w' to 'a' '''
+with open('{}e621-total-2021-11-05-a.json'.format(directory), 'w') as f:
 	f.write('[')
 	while max_id > 0:
 		''' When refreshing data, change page=b{} to page=a{} '''
 		r = requests.get('{}&page=b{}'.format(url, max_id), headers=e621_agent, auth=HTTPBasicAuth(login, api_key))
 		if r.status_code != 200:
-			print('Error got: {}'.format(r.status_code, r.text))
+			print('!!! 200 !!!')
 		if r.status_code == 429:
 			print('!!! 429 !!!')
 		if r.status_code == 503:
@@ -35,10 +36,13 @@ with open('{}e621-total-2021-10-25.json'.format(directory), 'w') as f:
 				continue
 			seen.append(item['file']['md5'])
 			#s.write(json.dumps(seen, indent=2))
-			print('#{} Dumped {}'.format(run, post_id))
+			print('#{} dumped {}'.format(run, post_id))
 			f.write(json.dumps(item))
 			f.write(',')
-		max_id -= 1000
+		''' When refreshing data, change -= 320 to += 320 '''
+		max_id -= 320
 		time.sleep(2)
 		run += 1
 	f.write(']')
+
+print('fetched {} records, with {} requests'.format(len(seen), run))
