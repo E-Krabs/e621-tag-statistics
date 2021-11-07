6888637d415796210db83301b409b3d93d2ec768
E. Krabs <Tyler.Smith11407@gmail.com>
2021-11-07 14:40:00 -0500

    Update tag_export.py

--- tag_export.py
+++ tag_export.py
@@ -4,16 +4,22 @@ import time
 import json
 
 directory = 'C:/Scripts/Python/[adjective][species]/'
-with open('{}JSON/e621-total-2021-10-25-a.json'.format(directory), 'r') as f:
+print("Loading JSON...")
+with open('{}JSON/e621-total-2021-11-05-a.json'.format(directory), 'rb') as f:
 	data = json.load(f)
 
 with open('{}JSON/tag-out.json'.format(directory), 'w') as o:
 	o.write('[')
-	run = 0
-	for item in data:
-		tags = item['tags']
-		o.write(json.dumps(tags))
+
+	print('Exporting...')
+	dic = {}
+	for key in data:
+		y = key['created_at'].split('-')[0]
+		m = key['created_at'].split('-')[1]
+		created_at = '{}-{}'.format(y, m)
+		tags = key['tags']
+		dic['created_at'] = created_at
+		dic['tags'] = tags
+		o.write(json.dumps(dic))
 		o.write(',')
-		print('Item #{}, Tags Dumped'.format(run))
-		run += 1
 	o.write(']')
