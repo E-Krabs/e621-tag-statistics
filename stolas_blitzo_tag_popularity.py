caeff10ad52b313038f85a96d9d9a18613ba12c1
E. Krabs <Tyler.Smith11407@gmail.com>
2021-11-10 10:06:44 -0500

    Update stolas_blitzo_tag_popularity.py

--- stolas_blitzo_tag_popularity.py
+++ stolas_blitzo_tag_popularity.py
@@ -15,7 +15,33 @@ with open('{}JSON/tag-out.json'.format(directory), 'r') as f:
 	data = json.load(f)
 
 	print('Counting...')
-	lst = []
+	dic = {}
+	for key in data:
+		general = key['tag']['general']
+		character = key['tag']['character']
+		if tag1 in character and tag2 in character:
+			for word in general:
+				if word not in dic:
+					dic[word] = 1
+				else:
+					dic[word] += 1
+			
+	
+	word_counter = collections.Counter(dic)
+	with open('{}{}_popular_tags.txt'.format(directory, tag1), 'w', encoding='utf-8') as o:
+		for word, count in word_counter.most_common():
+			o.write('{0}: {1}\n'.format(word, count))
+	
+	print('Plotting...')
+	lst = word_counter.most_common(display)
+	df = pd.DataFrame(lst, columns = ['Tag', 'Count'])
+	df.plot.bar(x='Tag',y='Count')
+	#plt.title('Stolas & Blitzo\'s Popular Tags')
+	#plt.show()
+	plt.savefig('{}{}_{}_popular_tags.png'.format(directory, tag1, tag2), dpi=300, bbox_inches='tight') #transparent=True
+	penis penis penis penis pp
+	
+	'''
 	for key in data:
 		characters = []
 		general = key['tags']['general']
@@ -28,3 +54,4 @@ with open('{}JSON/tag-out.json'.format(directory), 'r') as f:
 	for item in lst:
 		if tag1 in item and tag2 in item: #if stolas and blitzo in list
 				for key in data:
+	'''
