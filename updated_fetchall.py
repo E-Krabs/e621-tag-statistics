34366ca40aaebcc70faa903a4b45582291956856
E. Krabs <Tyler.Smith11407@gmail.com>
2021-12-01 21:56:58 -0500

    Update updated_fetchall.py

--- updated_fetchall.py
+++ updated_fetchall.py
@@ -13,14 +13,15 @@ e621_agent = {
 login = '' # your username
 api_key = '' # your api key
 
-max_id = 3011070 #3051712 3011070 2992268 518808
-seen = {}
+max_id = 3011070 #3053317 3011070 2992268 518808
+seen = {} #[]
 run = 1
 directory = 'C:/Scripts/Python/[adjective][species]/'
 ''' When refreshing data, change 'w' to 'a' '''
 with open('{}JSON/e621-total-2021-11-05-a.json'.format(directory), 'a') as f:
-	''' When refreshing data, remove f.write('[') '''
-	while max_id > 0:
+	''' When refreshing data, replace f.write('[') to (',') '''
+	f.write(',')
+	while True:
 		while_start = time.time()
 		''' When refreshing data, change page=b{} to page=a{} '''
 		r = requests.get('{}&page=a{}'.format(url, max_id), headers=e621_agent, auth=HTTPBasicAuth(login, api_key))
@@ -36,15 +37,18 @@ with open('{}JSON/e621-total-2021-11-05-a.json'.format(directory), 'a') as f:
 			if md5 in seen:
 				continue
 			seen[md5] = 1
+			#seen.append(item['file']['md5'])
 			print('#{} dumped {}'.format(run, post_id))
-			f.write(json.dumps(item) + '\n')
+			f.write(json.dumps(item)) #+ '\n'
 			f.write(',')
 		''' When refreshing data, change -= 320 to += 320 '''
-		max_id += 320
 		time.sleep(2)
-		run += 1
 		now = time.time()
-		print('Loop {}: {}'.format(run-1, now-while_start))
+		if now-while_start < 3:
+			break
+		print('Loop {}: {}'.format(run, now-while_start))
+		max_id += 320
+		run += 1
 	f.write(']')
 
-print('Fetched {} records, with {} requests'.format(len(seen), run))
+print('Fetched {} records, with {} requests'.format(len(seen), run-1))
