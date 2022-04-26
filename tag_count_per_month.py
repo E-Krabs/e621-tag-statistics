02c18d1e4b8be100cc89bbed448b525c438b2d6f
Eugene Krabs <tyler.smith11407@gmail.com>
2022-04-25 21:22:36 -0400

    Update tag_count_per_month.py

--- tag_count_per_month.py
+++ tag_count_per_month.py
@@ -13,8 +13,16 @@ colors = cycle(['blue', 'aqua', 'yellow', 'red', 'pink', 'brown', 'grey', 'purpl
 omit_final = True
 omit_empty = False
 cwd = os.getcwd()
-print('connecting to database')
-db = sqlite3.connect('{}/JSON/jsql.sqlite'.format(cwd))
+
+for file in os.listdir('.'):
+	if fnmatch(file, '*.sqlite') == True:
+		sqlite_file = cwd + '\\' + file
+		break
+else:
+	print('Please generate the database.')
+	exit()
+	
+print('Connecting to {}'.format(file))
 cursor = db.cursor()
 fetch_query = "SELECT created_at, tags FROM e621"
 cursor.execute(fetch_query)
